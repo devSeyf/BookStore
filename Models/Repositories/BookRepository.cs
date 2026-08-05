4a0eb7b7e978bc337761e271b1842fe985242e42
devSeyf <ssaaiiffjjaammaall@gmail.com>
2026-08-05 18:17:58 +0300

    Proje dosyası ekle.

---
+++ Models/Repositories/BookRepository.cs
@@ -0,0 +1,30 @@
+﻿namespace BookStore.Models.Repositories
+{
+    public class BookRepository : IBookstoreRepository<Book>
+    {
+        public void Add(Book entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Book> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Book entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Models/Repositories/IBookstoreRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace BookStore.Models.Repositories
+{
+    public interface IBookstoreRepository <TEntity>
+    {
+        IList<TEntity> List();
+
+        TEntity Find(int id);
+
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+
+        void Delete(int id);
+
+    }
+}
